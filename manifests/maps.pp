245646b0ca3306a3479e3727f2016b047b082193
Balazs Nagy <julsevern@gmail.com>
2013-04-23 18:52:39 +0200

    postfix::maps

---
+++ manifests/maps.pp
@@ -0,0 +1,44 @@
+class postfix::maps(
+  $ensure = 'present',
+  $dbhost,
+  $dbname,
+  $dbuser,
+  $dbpass,
+  $aliases_table = 'aliases',
+  $aliases_destination = 'destination',
+  $aliases_local = 'local',
+  $aliases_domain = 'domain',
+  $domains_table = 'domains',
+  $domains_domain = 'domain',
+  $users_table = 'users',
+  $users_user = 'user',
+  $users_domain = 'domain'
+  ) inherits postfix {
+
+  include postfix::config
+
+  File {
+    ensure  => $ensure,
+    owner => 'root',
+    group => 'postfix',
+    mode  => '0640'
+  }
+
+  file {
+    "${postfix::maps}/domain_forwardings.cf":
+      content => template("postfix/domain_forwardings_map.erb")
+      ;
+    "${postfix::maps}/domains.cf":
+      content => template("postfix/domains_map.erb")
+      ;
+    "${postfix::maps}/email2email.cf":
+      content => template("postfix/email2email_map.erb")
+      ;
+    "${postfix::maps}/forwardings.cf":
+      content => template("postfix/forwardings_map.erb")
+      ;
+    "${postfix::maps}/mailboxes.cf":
+      content => template("postfix/mailboxes_map.erb")
+      ;
+  }
+}
---
+++ spec/classes/maps_spec.rb
@@ -0,0 +1,93 @@
+require 'spec_helper'
+
+describe 'postfix::maps' do
+  let(:params) do
+    {
+      ensure: 'present',
+      dbhost: 'dbhost',
+      dbname: 'dbname',
+      dbuser: 'dbuser',
+      dbpass: 'dbpass',
+      aliases_table: 'aliases',
+      aliases_destination: 'destination',
+      aliases_local: 'local',
+      aliases_domain: 'domain',
+      domains_table: 'domains',
+      domains_domain: 'domain',
+      users_table: 'users',
+      users_user: 'user',
+      users_domain: 'domain',
+    }
+  end
+
+  it {should include_class('postfix')}
+
+  it 'generates domain_forwardings map' do
+    should contain_file('/etc/postfix/maps/domain_forwardings.cf').with(
+      owner: 'root',
+      group: 'postfix',
+      mode: '0640'
+    ).
+      with_content(%r{^user = dbuser$}).
+      with_content(%r{^password = dbpass$}).
+      with_content(%r{^hosts = dbhost$}).
+      with_content(%r{^dbname = dbname$}).
+
+      with_content(%r{^query = SELECT `destination` FROM `aliases` WHERE `local`='' AND `domain`=SUBSTRING_INDEX\('%s', '@', -1\)$})
+  end
+
+  it 'generates domains map' do
+    should contain_file('/etc/postfix/maps/domains.cf').with(
+      owner: 'root',
+      group: 'postfix',
+      mode: '0640'
+    ).
+      with_content(%r{^user = dbuser$}).
+      with_content(%r{^password = dbpass$}).
+      with_content(%r{^hosts = dbhost$}).
+      with_content(%r{^dbname = dbname$}).
+      with_content(%r{^query = SELECT 'virtual' from `domains` WHERE `domain`='%s'$})
+  end
+
+  it 'generates email2email map' do
+    should contain_file('/etc/postfix/maps/email2email.cf').with(
+      owner: 'root',
+      group: 'postfix',
+      mode: '0640'
+    ).
+      with_content(%r{^user = dbuser$}).
+      with_content(%r{^password = dbpass$}).
+      with_content(%r{^hosts = dbhost$}).
+      with_content(%r{^dbname = dbname$}).
+
+      with_content(%r{^query = SELECT CONCAT\(user, '@', domain\) FROM users WHERE user='%u' AND domain='%d'$})
+  end
+
+  it 'generates forwardings map' do
+    should contain_file('/etc/postfix/maps/forwardings.cf').with(
+      owner: 'root',
+      group: 'postfix',
+      mode: '0640'
+    ).
+      with_content(%r{^user = dbuser$}).
+      with_content(%r{^password = dbpass$}).
+      with_content(%r{^hosts = dbhost$}).
+      with_content(%r{^dbname = dbname$}).
+
+      with_content(%r{^query = SELECT `destination` FROM `aliases` WHERE `local`='%u' AND `domain`='%d'$})
+  end
+
+  it 'generates mailboxes map' do
+    should contain_file('/etc/postfix/maps/mailboxes.cf').with(
+      owner: 'root',
+      group: 'postfix',
+      mode: '0640'
+    ).
+      with_content(%r{^user = dbuser$}).
+      with_content(%r{^password = dbpass$}).
+      with_content(%r{^hosts = dbhost$}).
+      with_content(%r{^dbname = dbname$}).
+
+      with_content(%r{^query = SELECT CONCAT\(`domain`, '/', `user`, '/'\) FROM `users` WHERE `user`='%u' AND `domain`='%d'$})
+  end
+end
